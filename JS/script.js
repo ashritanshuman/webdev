583068e555bb4048b8247ac53816c44ca0f794f3
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-04-12 16:51:54 +0530

    js init

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+// variables & constants
+
+var names = "ashrit"
+var rollno = 12345
+
+console.log( names + "@" + rollno)
+
+// var let
+
+
+
+// hoisting we can use the calling function just above the the variable declaration and init of vaaaaariable
+
+// array
+
+var a = [12,13,14];
+var b = a;
+
+b.pop();
+
+console.log(b)
