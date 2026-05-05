4aedda2be76441ba46f4e7fb8dc054b95a03f9ef
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-05 17:44:17 +0530

    feat: add datatype.js and printing.js with data type examples and console methods

---
+++ JS/1/datatype.js
@@ -0,0 +1,44 @@
+// DATA TYPES IN JAVASCRIPT:
+
+console.log("DATA TYPES IN JAVASCRIPT:");
+
+/*
+String - names and nouns
+Number - integers
+Boolean -true or false | yes or no 
+bigint - for very large integers
+
+undefined - for variables that have been declared but not assigned a value
+null - for variables that have been explicitly set to have no value or simply empty value
+
+symbol - for unique identifiers
+
+OOPS
+
+*/ 
+
+// var score = 102  not in morden codebases
+
+let name = "ashrit";
+let score = 101;
+let isLoggedIn = false;
+
+// console.log(typeof name);
+// console.log(typeof score);
+// console.log(typeof isLoggedIn);
+
+// OBJECTS IN JAVASCRIPT:
+//  ARRAYS
+let teaTypes = ["greeem tea", "black tea", "oolong tea"];
+let user = {
+    firstname: "ashrit",
+    lastname: "anshuman",
+    age: 22,
+}
+
+
+// Borrowing a method from an array to use on an object
+let getScore = score;
+
+console.log(getScore);
+
---
+++ JS/1/printing.js
@@ -0,0 +1,11 @@
+console.log("hello world");
+// process.stdout.write("hello world"); --- doesn't go to new line
+
+console.table({ name: "Alice", age: 30 });  // table format
+console.error("This is an error message");
+
+console.count("Counter");  // Counter: 1
+console.count("Counter");  // Counter: 2
+console.time("Timer");
+
+console.clear
