548b04403a913c38e39e2e8de0b42bb136d86e01
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-05 17:44:42 +0530

    feat: add changes.js with examples of variable assignment and constant usage

---
+++ JS/1/changes.js
@@ -0,0 +1,15 @@
+let gameName = "COD" ;
+
+gameName = "PUBG";   // override
+
+console.log(gameName);
+
+
+const username = "ashritanshuman";
+
+username = "ashrit";  // can't override a constant variable
+
+// username = "ashrit";  not allowed to override a constant variable
+
+console.log(username);
+
