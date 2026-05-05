933b60499dae52dcd5f5f6e0ed836a630f396aef
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-05 17:44:53 +0530

    feat: add operations.js with basic arithmetic and comparison examples

---
+++ JS/1/operations.js
@@ -0,0 +1,36 @@
+let add = 4 + 5;
+let subtract = 100 - 25;
+let multiply = 5 * 5;
+let divide = 100 / 5;
+let reminder = 2 % 5;
+let exponential = 2 ** 5;
+
+console.log(exponential);
+
+
+
+let bonous = 25;
+let score = 100;
+
+let totalScore = score + bonous;
+
+console.log(totalScore);
+
+let myScore = 110;
+
+myScore = myScore ++ ;
+myScore = ++myScore ;
+
+console.log(myScore);
+
+let num1 = 2
+let num2 = 5
+let num3 = 10
+
+console.log("compare: " + (num1 == num2));
+console.log("compare: " + (num1 != num2));
+console.log("compare: " + (num1 < num2));
+console.log("compare: " + (num1 > num2));
+console.log("compare: " + (num1 <= num2));
+console.log("compare: " + (num1 >= num2));
+
