c318a29cdc6bb85dcea42d7aafc948af00f8809f
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-05 17:45:01 +0530

    feat: add logical.js with examples of logical operators and user verification

---
+++ JS/1/logical.js
@@ -0,0 +1,17 @@
+//  & -> AND
+// || -> OR
+//  ! -> NOT / REVERSAL --- if true then it becomes false and if false then it becomes true
+
+let isVerified = true;
+let hasPaymentMethod = true;
+let isGuest = false;
+
+// console.log(isVerified && isGuest);  
+// console.log(isVerified || isGuest);
+// console.log(!isGuest); // TRUE
+
+let isemailUser = true;
+let isGoogleUser = false;
+
+console.log(isemailUser || isGoogleUser); // TRUE
+
