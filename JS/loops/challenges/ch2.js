ca443df7aa4471de1b88e93f82f749477ab02712
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-07 12:10:46 +0530

    feat: add ch2.js with a loop example filtering visited cities

---
+++ JS/loops/challenges/ch2.js
@@ -0,0 +1,16 @@
+// 2.
+
+let cities = ["new york", "los angeles", "chicago", "houston", "phoenix"];
+
+let visitedCities = [];
+
+
+for (let i = 0; i < cities.length; i++) {
+    
+    if (cities[i] === "chicago" || cities[i] === "Chicago") {
+        continue;
+    }
+
+    visitedCities.push(cities[i]);
+}
+console.log(visitedCities);
