f8143c4423da500719a0632a6d63e75beac2e885
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-07 12:10:40 +0530

    feat: add challenge files for for loop and for-of loop examples in JavaScript

---
+++ JS/loops/challenges/ch1.js
@@ -0,0 +1,16 @@
+// 1. for loop 
+
+let teas = ["green tea", "black tea", "oolong tea", "white tea"];
+
+let selectedTeas = [];
+
+for (let i = 0; i < teas.length; i++) {
+
+    if (teas[i] === "oolong tea") {
+        break;
+    }
+    selectedTeas.push(teas[i]);
+
+}
+console.log(selectedTeas);
+
