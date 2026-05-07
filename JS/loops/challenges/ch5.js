dc7173ede05426d7bc9a31e3fea6e437fcdc7586
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-07 23:22:36 +0530

    ch 5

--- JS/loops/challenges/ch5.js
+++ JS/loops/challenges/ch5.js
@@ -1,26 +1,24 @@
-// objects
-
+// objects programs:
 let citiesPopulation = {
-    London : 8900000,
-    "New York" : 5660000,
-    Paris : 450000,
-    Berlin : 4500045
+    London: 8900000,
+    "New York": 5660000,
+    Paris: 450000,
+    Berlin: 4500045
 };
-
-
-let newCityPopulation = {}  //empty object
-
 // console.log(Object.keys(citiesPopulation));
 // console.log(Object.values(citiesPopulation));
 
+let newCityPopulation = {}  //empty object
+
 for (const city in citiesPopulation) {
     console.log(citiesPopulation[city]);
+
+    if (city === "Berlin") {
+        break;
+    }
     
-    // if (city === "Berlin") {
-    //     break
-    // }
-    
-    newCityPopulation[city] = citiesPopulation[city];    
+    newCityPopulation[city] = citiesPopulation[city];
 }
 
-console.log(newCityPopulation);
+console.log(newCityPopulation)
+
