4c1d07af82c122da33d515fd5edbde491b82b6c3
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-07 23:22:45 +0530

    ch6

--- JS/loops/challenges/ch6.js
+++ JS/loops/challenges/ch6.js
@@ -1,7 +1,19 @@
+
 let worldCities = {
-    London : 8900000,
-    "New York" : 5660000,
-    Paris : 450000,
-    Berlin : 4500045
+        London: 8900000,
+        NewYork: 5660000,
+        Paris: 45000,
+        Berlin: 4500045
 };
 
+let largeCities = {};
+
+for (const city in worldCities) {
+    if (worldCities[city] < 4500000)
+        continue
+
+    largeCities[city] = worldCities[city];
+
+}
+
+console.log(largeCities);
