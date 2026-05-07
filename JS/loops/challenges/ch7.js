e019cd0966b8ad9975bd61cf271b35ec0f139256
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-07 23:51:03 +0530

    ch 7

---
+++ JS/loops/challenges/ch7.js
@@ -0,0 +1,27 @@
+let teaCollection = ["greeen tea", "oolong tea", "chai", "black tea" ];
+
+let availableTeas = [];
+
+teaCollection.forEach(function(tea) {
+    if (tea === "chai") {
+        // break; // error
+
+        return 1;
+    }
+
+    availableTeas.push(tea);
+
+});
+
+console.log(availableTeas);
+
+// function hello() {
+//     console.log("abc")
+
+// }
+
+// () => {
+//     console.log("abc");
+    
+// }
+
