7711e006f1bd606153e7f8a8f2017a2576e775d4
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-07 12:10:32 +0530

    feat: enhance loops.js with detailed examples of loop structures in JavaScript

--- JS/loops/loops.js
+++ JS/loops/loops.js
@@ -1,9 +1,87 @@
 //  LOOPS:
 
-// for (initialization; condition; afterthought)
-//   statement
+/* for (initialization; condition; iteration) {
+  body of the loop
+} */
 
-for (let step = 0; step < 5; step++) {
-  console.log("hello ashu");
+// 1. 
+let sum = 0;
+
+for (let i = 1; i <= 5; i++) {
+  sum += i;
+
+}
+console.log(sum);
+
+
+let i = 0;
+let s = 0;
+
+while (i <= 5) {
+  s += i;
+  i++;
+
+}
+console.log(s);
+
+
+// 2.
+let countdown = [];
+let j = 1;
+
+while (j<=5) {
+  countdown.push(j);
+  j++;
+
 }
 
+console.log(countdown);
+
+// 3.
+
+
+/* let teaCollection = [];
+let tea;
+
+do {
+  tea = prompt("enter your favourite tea: ")
+
+  if (tea !== "stop") {
+    teaCollection.push(tea);
+  }
+} while (tea !== "stop"); */
+
+/* types: 
+1. for loop
+2. while loop
+3. do...while loop
+4. for...in loop
+5. for...of loop 
+
+*/
+
+// 4.
+
+let multipliednums = [];
+let nums = [1, 2, 3, 4, 5];
+
+let k = 2;
+
+for (let i = 0; i < nums.length; i++) {
+  multipliednums.push(nums[i] * k);
+  // const element = nums[i];
+}
+console.log(multipliednums);
+
+// 5.
+
+let cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+
+let cityList = [];
+
+for (let i = 0; i < cities.length; i++) {
+  const myCity = cities[i];
+  cityList.push(myCity);
+}
+
+console.log(cityList);
