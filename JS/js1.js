0db8f040ef40e4ed593792670dcc316018a75a2b
Ashrit Anshuman Pradhan <ashrit99999@gmail.com>
2026-05-09 20:18:59 +0530

    jsp

---
+++ JS/js1.js
@@ -0,0 +1,17 @@
+let obj = {
+    id: "ashritanshuman",
+    name: "Ashrit Anshuman Pradhan",
+    age: 19,
+    phoneNumber: 6370205656
+
+}
+
+let onj1 = obj;
+
+onj1.name = "AShrit"
+console.log(onj1);
+console.log(obj);
+
+
+
+let age = 19;
